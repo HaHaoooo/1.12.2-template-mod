7632fc7b5c08768e21d1b4d3de9f882c9e73b61e
HaHaoooo <xyx20180325@gmail.com>
2024-05-07 13:49:32 -0700

    5/7

--- src/main/java/com/lingxian/test/command/ChangeDamage.java
+++ src/main/java/com/lingxian/test/command/ChangeDamage.java
@@ -4,6 +4,7 @@ import com.lingxian.test.Registry;
 import com.lingxian.test.entity.CoinEntity;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
@@ -47,7 +48,11 @@ public class ChangeDamage extends CommandBase {
         // 修改物品的属性（可以扔的硬币）
         if (itemName.equals("test:throw_coin")) {
             CoinEntity.attackDamage = damageAmount;
-            Style style = new Style().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("这是聊天框高级悬停显示")));
+            EntityPlayer player = null;
+            if (sender instanceof EntityPlayer){
+                player = (EntityPlayer) sender;
+            }
+            Style style = new Style().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(new ItemStack(Registry.throwCoin).getTooltip(player, () -> false).get(1))));
             TextComponentString string1 = new TextComponentString("物品[");
             ITextComponent string2 = new TextComponentString(TextFormatting.YELLOW + Registry.throwCoin.getItemStackDisplayName(new ItemStack(Registry.throwCoin)) + TextFormatting.WHITE).setStyle(style);
             TextComponentString string3 = new TextComponentString("]的投掷伤害已修改为" + damageAmount);
